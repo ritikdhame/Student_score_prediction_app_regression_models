a4a85d4237b32c04d76c9fedfd2d920c9b541a90
ritikdhame <ritikdhame@gmail.com>
2023-06-22 13:04:52 -0400

    added model trainer picke file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,8 +1,8 @@
-import os 
-import sys 
+import os
+import sys
 from src.exception import CustomException
-from src.logger import logging 
-import pandas as pd 
+from src.logger import logging
+import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -10,7 +10,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -25,20 +26,20 @@ class DataIngestion:
         logging.info("Entered the data ingestion method or component")
         try:
             df=pd.read_csv('notebook\data\stud.csv')
-            logging.info('Read the dataset as dataframe')#reading data set
+            logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
-            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)#converting to csv with raw data path
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
             logging.info("Train test split initiated")
-            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)#performing train test split
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Ingestion of the data iss completed")
+            logging.info("Inmgestion of the data iss completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -57,6 +58,3 @@ if __name__=="__main__":
 
     modeltrainer=ModelTrainer()
     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
-
-
-
